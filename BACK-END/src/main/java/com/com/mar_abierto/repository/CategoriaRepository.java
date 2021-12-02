4ed8a27dfb1c374dc793675ecf2bb9e822b32ad8
DiegoCardenasUNAB <diego.cardenas2@o365.unab.edu.co>
2021-12-01 20:17:59 -0500

    Eliminado archivos

--- BACK-END/src/main/java/com/com/mar_abierto/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.com.mar_abierto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.com.mar_abierto.entity.CategoriaEntity;
-
-public interface CategoriaRepository extends JpaRepository<CategoriaEntity, Long>{
-
-}
--- BACK-END/src/main/java/com/com/mar_abierto/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.com.mar_abierto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.com.mar_abierto.entity.ProductoEntity;
-
-public interface ProductoRepository extends JpaRepository<ProductoEntity, Long>{
-
-}
