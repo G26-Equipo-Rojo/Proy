b06d82e8e3928f2cd9789bca2397c4720a89d374
DiegoCardenasUNAB <diego.cardenas2@o365.unab.edu.co>
2021-12-05 22:47:24 -0500

    Eliminado archivos

--- BACK-END/MAR_ABIERTO_C4/src/main/java/com/unab/MAR_ABIERTO_C4/Repository/IUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unab.MAR_ABIERTO_C4.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.unab.MAR_ABIERTO_C4.Modelo.UsuarioModelo;
-
-public interface IUsuarioRepository extends MongoRepository<UsuarioModelo, String>{
-
-}
